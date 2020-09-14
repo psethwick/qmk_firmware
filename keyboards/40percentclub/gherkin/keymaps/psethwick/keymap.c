6e5274e143984ae5b255e00f22e14d4c85803c19
Seth Rider <seth@thumbquat.com>
2020-09-14 18:04:57 +0100

    keymap fixes

--- keyboards/40percentclub/gherkin/keymaps/psethwick/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/psethwick/keymap.c
@@ -1,41 +1,41 @@
 #include QMK_KEYBOARD_H
 #include "keymap_steno.h"
 
-#define FN1_N       LT(1, KC_N)
-#define FN2_B       LT(2, KC_B)
-#define FN3_C       LT(3, KC_C)
-#define FN4_V       LT(4, KC_V)
-#define FN5_B       LT(5, KC_B)
-#define CTL_Z       CTL_T(KC_Z)
-#define ALT_X       ALT_T(KC_X)
-#define ALT_BSPC    ALGR_T(KC_BSPC)
-#define CTL_SPC     RCTL_T(KC_SPC)
-#define SFT_ENT     RSFT_T(KC_ENT)
+#define L_Z       LT(5, KC_Z)
+#define L_X       LT(4, KC_X)
+#define L_C       LT(3, KC_C)
+#define L_V       LT(2, KC_V)
+#define L_N       LT(1, KC_N)
+#define L_B       LT(1, KC_B)
+#define L_M       LT(2, KC_M)
+#define L_COMM    LT(3, KC_COMM)
+#define L_DOT     LT(4, KC_DOT)
+#define L_SLSH    LT(5, KC_SLSH)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ortho_3x10(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    LGUI_T(KC_A),    ALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    KC_G,    KC_H,    RSFT_T(KC_J),    RCTL_T(KC_K),    ALGR_T(KC_L),    RGUI_T(KC_ESC),
-    CTL_Z,   KC_X,   FN3_C,   FN4_V,   FN2_B,   FN1_N,   FN5_B,   KC_BSPC,KC_SPC, KC_ENT
+    LGUI_T(KC_A),     ALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    KC_G,    KC_H,    RSFT_T(KC_J),    RCTL_T(KC_K),    ALGR_T(KC_L),    RGUI_T(KC_SCLN),
+    L_Z,     L_X,    L_C,     L_V,     L_B,     L_N,     L_M,      L_COMM,   L_DOT,  L_SLSH
   ),
 
   [1] = LAYOUT_ortho_3x10(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, TO(6)
-  ),
-
-  [2] = LAYOUT_ortho_3x10(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, KC_GRV,
+    KC_BSPC, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC,
+    _______, _______, _______, KC_ENT,  KC_SPC,  KC_SPC,  KC_ENT,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  [3] = LAYOUT_ortho_3x10(
+  [2] = LAYOUT_ortho_3x10(
     _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_TAB,  _______, _______, _______, _______, KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN, KC_QUOT,
     _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  ),
+
+  [3] = LAYOUT_ortho_3x10(
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, TO(6)
   ),
 
   [4] = LAYOUT_ortho_3x10(
@@ -81,3 +81,6 @@ void led_set_user(uint8_t usb_led) {
 void matrix_init_user() {
   steno_set_mode(STENO_MODE_GEMINI);
 }
+
+    //KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    //KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, KC_GRV,
